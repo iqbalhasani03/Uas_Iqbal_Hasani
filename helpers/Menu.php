d694d865c1d5a27829fa82745ba9eeb571249172
iqbalhasani03 <148632473+iqbalhasani03@users.noreply.github.com>
2024-01-04 11:26:16 +0700

    Add files via upload

---
+++ helpers/Menu.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => '<i class="fa fa-home "></i>'
+		),
+		
+		array(
+			'path' => 'dokter', 
+			'label' => 'Dokter', 
+			'icon' => '<i class="fa fa-user-md "></i>'
+		),
+		
+		array(
+			'path' => 'pasien', 
+			'label' => 'Pasien', 
+			'icon' => '<i class="fa fa-user "></i>'
+		),
+		
+		array(
+			'path' => 'user', 
+			'label' => 'User', 
+			'icon' => '<i class="fa fa-users "></i>'
+		),
+		
+		array(
+			'path' => 'obat', 
+			'label' => 'Obat', 
+			'icon' => '<i class="fa fa-battery-4 "></i>'
+		),
+		
+		array(
+			'path' => 'ruangan', 
+			'label' => 'Ruangan', 
+			'icon' => '<i class="fa fa-hospital-o "></i>'
+		),
+		
+		array(
+			'path' => 'rekam_medis', 
+			'label' => 'Rekam Medis', 
+			'icon' => '<i class="fa fa-files-o "></i>'
+		),
+		
+		array(
+			'path' => 'role_permissions', 
+			'label' => 'Role Permissions', 
+			'icon' => '<i class="fa fa-gear "></i>'
+		),
+		
+		array(
+			'path' => 'roles', 
+			'label' => 'Roles', 
+			'icon' => '<i class="fa fa-gears "></i>'
+		)
+	);
+		
+	
+	
+			public static $JK = array(
+		array(
+			"value" => "L", 
+			"label" => "L", 
+		),
+		array(
+			"value" => "P", 
+			"label" => "P", 
+		),);
+		
+}
